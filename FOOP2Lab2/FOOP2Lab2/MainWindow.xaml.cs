9ab95b617ac86080055ef03bb0ef3423697991d7
S00173244 <S00173244@mail.itsligo.ie>
2018-01-31 15:59:37 +0000

    Not finished.

--- FOOP2Lab2/FOOP2Lab2/MainWindow.xaml.cs
+++ FOOP2Lab2/FOOP2Lab2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace FOOP2Lab2
 {
@@ -20,9 +22,69 @@ namespace FOOP2Lab2
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rn = new Random();
+        List<Player> playerList = new List<Player>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            CreatePlayers();
+            lbox_playerList.ItemsSource = playerList;
+        }
+
+        private void CreatePlayers()
+        {
+            DateTime startDate = new DateTime(2000, 1, 1);
+            for (int i = 0; i < 20; i++)
+            {
+
+                int range = (DateTime.Now - startDate).Days;
+                playerList.Add(new Player("Player" + i, startDate.AddDays(rn.Next(range))));
+
+            }
+        }
+
+        private void btn_AddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if(tbox_Name.ToString()!= null )
+            {
+                DateTime dt = dp_DateOfBirth.SelectedDate.Value;
+                playerList.Add(new Player(tbox_Name.Text, dt));
+                lbox_playerList.ItemsSource = "";
+                lbox_playerList.ItemsSource = playerList;
+
+                
+                
+            }
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            string json = JsonConvert.SerializeObject(playerList);
+            Console.WriteLine(json);
+            FileStream fs = new FileStream("players.json", FileMode.Append);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.Write(json);
+
+            sw.Dispose();
+            sw.Close();
+            fs.Dispose();
+            fs.Close();
+        }
+
+        private void btn_Load_Click(object sender, RoutedEventArgs e)
+        {
+            FileStream fs = new FileStream("players.json", FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+            List<Player> pl = (List < Player > )JsonConvert.DeserializeObject(sr.ReadLine());
+
+            sr.Dispose();
+            sr.Close();
+            fs.Dispose();
+            fs.Close();
         }
     }
 }
---
+++ FOOP2Lab2/FOOP2Lab2/Player.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOOP2Lab2
+{
+    class Player
+    {
+        private string name;
+        private DateTime dateOfBirth;
+        private string category;
+        private int age;
+
+        #region Accessors
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return dateOfBirth;
+            }
+
+            set
+            {
+                dateOfBirth = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+
+            set
+            {
+                age = value;
+            }
+        }
+
+        #endregion
+
+        public Player(string n, DateTime dob)
+        {
+            Name = n;
+            DateOfBirth = dob;
+            Age = ((new DateTime(2018, 8, 31) - DateOfBirth).Days)/365;
+            
+            Console.WriteLine(Age);
+            setCategory(Age);
+        }
+
+        private void setCategory(int age)
+        {
+            if (age <= 10) category = "u10";
+            else if (age <= 12) category = "u12";
+            else if (age <= 14) category = "u14";
+            else if (age <= 16) category = "u16";
+            else if (age <= 18) category = "u18";
+            else category = "Senior";
+        }
+
+
+        public override string ToString()
+        {
+
+            string catText = "";
+            if (category == "u10") catText = "UNDER 10";
+            else if (category == "u12") catText = "UNDER 12";
+            else if (category == "u14") catText = "UNDER 14";
+            else if (category == "u16") catText = "UNDER 16";
+            else if (category == "u18") catText = "UNDER 18";
+            else if (category == "Senior") catText = "Above 18";
+
+
+            return string.Format("{0}, Age {1} ({2})",Name,Age,catText);
+        }
+    }
+}
